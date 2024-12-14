7dc8d57a2f83b3a5c6484e66e7943a2dec0d0127
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-12-14 18:36:48 +0600

    chore: deleted unused file

--- internal/services/storage.go
+++
@@ -1,9 +0,0 @@
-package services
-
-// import "github.com/itaraxa/turbo-waddle/internal/config"
-
-// type Storage struct{}
-
-// func NewStorage(c *config.GopherMartConfig) (*Storage, error) {
-// 	return &Storage{}, nil
-// }
