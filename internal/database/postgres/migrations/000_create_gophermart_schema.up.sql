aeaa668bf9196dc5ab9ecfbbbb1f659a12f4b07d
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-12-17 23:59:08 +0600

    feat: add order_status field in gophermat.orders table

--- internal/database/postgres/migrations/000_create_gophermart_schema.up.sql
+++ internal/database/postgres/migrations/000_create_gophermart_schema.up.sql
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS gophermart.orders (
     order_id TEXT NOT NULL UNIQUE,
     order_sum DECIMAL,
     processed_at TIMESTAMPTZ,
+    order_status TEXT,
     user_id INTEGER,
     PRIMARY KEY ( order_id ),
     FOREIGN KEY ( user_id ) REFERENCES gophermart.users ( user_id )
